e565b79474245c0b7c7c472008f75d97716f1242
Guilherme Silva Ferreira <guilherme.silva.ferreira@outlook.pt>
2025-07-27 10:09:28 +0000

    Alteraçoes no login do usuario

--- javascript/js_css.js
+++ javascript/js_css.js
@@ -19,10 +19,27 @@ function login() {
     if (resposta) {
         // O utilizador clicou em OK
         let nome_user = window.prompt("Escreva o seu nome de utilizador");
-        document.getElementById('paragrafo.login').textContent = nome_user;
+        document.getElementById("login").style.display = "none";
+        console.log("Botãobotao_login(); de login ocultado.");
+        window.alert("Bem-vindo, " + nome_user + "!");
+        document.getElementById('paragrafo.login').textContent = "Ola " + nome_user + "!"
         console.log("Login efetuado com o nome: " + nome_user);
     } else {
         // O utilizador clicou em Cancelar
         console.log("Login cancelado pelo utilizador.");
+    }
+}
+
+function terminar_sesao() {
+    const resposta = window.confirm('prestes a terminar a sessão. Continuar?');
+    if (resposta) {
+        // O utilizador clicou em OK
+        document.getElementById("login").style.display = "block";
+        console.log("Botão de login visível novamente.");
+        document.getElementById('paragrafo.login').textContent = "";
+        console.log("Sessão terminada e nome de utilizador removido.");
+    } else {
+        // O utilizador clicou em Cancelar
+        console.log("Terminar sessão cancelado pelo utilizador.");
     }
 }
